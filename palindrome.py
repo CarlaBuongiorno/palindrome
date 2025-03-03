529d3c7f23dc2a16817b9e0ce35582e87c9b69e0
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-03 13:43:49 +0100

    Add main palindrome function with test file

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+# Write a function that takes a string,
+# and returns True if that string is a palindrome.
+
+def main():
+    palindrome = is_palindrome()
+    print(palindrome)
+
+
+def is_palindrome():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_palindrome.py
@@ -0,0 +1,5 @@
+from palindrome import is_palindrome
+
+
+def test_function_exists():
+    assert is_palindrome
