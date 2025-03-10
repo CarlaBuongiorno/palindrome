82d164d45f9ff046487ef6ea541b5260378af061
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-10 14:35:40 +0100

    Write tests and function to check validity of user input

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,21 @@
-# Write a function that takes a string,
-# and returns True if that string is a palindrome.
+'''Write a function that takes a string, 
+and returns True if that string is a palindrome.'''
 
 def main():
-    palindrome = is_palindrome()
+    user_input = input("Type your palindrome: ")
+    is_valid = check_if_valid(user_input)
+    if is_valid:
+        palindrome = is_palindrome(user_input)
     print(palindrome)
 
 
-def is_palindrome():
+def check_if_valid(user_input):
+    if not isinstance(user_input, str):
+        raise TypeError('You have entered the wrong data type. A string is needed.')
+    return user_input
+
+
+def is_palindrome(user_input):
     pass
 
 
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,5 +1,17 @@
-from palindrome import is_palindrome
+import pytest
+
+from palindrome import (
+    check_if_valid,
+    is_palindrome,
+    )
 
 
 def test_function_exists():
     assert is_palindrome
+
+def test_user_input_is_valid():
+    assert check_if_valid('hello') == 'hello'
+
+def test_user_input_not_valid():
+    with pytest.raises(TypeError, match='You have entered the wrong data type. A string is needed.'):
+        check_if_valid(123)
