57596e0d3b728e7a06fbf9c8794e280617daf7dc
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-11 21:18:45 +0100

    Refactor is_palindrome()

--- pure_palindrome.py
+++ pure_palindrome.py
@@ -1,8 +1,6 @@
 def is_palindrome(user_input):
     cleaned_user_input = _clean_input(user_input)
-    if cleaned_user_input != cleaned_user_input[::-1]:
-        return False
-    return True
+    return cleaned_user_input == cleaned_user_input[::-1]
 
 
 def _clean_input(user_input): # private functions are not tested because they are an implementation detail of the public functions
